9476b2d0e61a5ea259f4ac337219b9f3d2a702f3
Jake P <76881086+jbped@users.noreply.github.com>
2021-08-31 15:51:04 -0600

    implemented hooks for categories

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,24 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { capitalizeFirstLetter } from '../../utils/helpers'
 
-const categories = [
-    {
-        name: "Commercial",
-        description:
-          "Photos of grocery stores, food trucks, and other commercial projects",
-      },
-      { name: "Portraits", description: "Portraits of people in my life" },
-      { name: "Food", description: "Delicious delicacies" },
-      {
-        name: "Landscape",
-        description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-      }
-]
-
-const categorySelected = (name) => {
-    console.log(`${name} clicked`)
-}
-
-const Nav = () => {
+const Nav = (props) => {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
     return (
         <header>
             <h2>
@@ -39,9 +30,9 @@ const Nav = () => {
                         </span>
                     </li>
                     {categories.map((category) => (
-                        <li className="mx-1" key={category.name}>
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+                            <span onClick={() => setCurrentCategory(category)}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
